95fcb8c0a21f13ad53453e0d8ad7d2c4a1ee003d
Oscar Mesa <becario.oscar.mesa@agileinnova.org>
2021-02-19 15:26:32 -0500

    Primera parametrización

---
+++ backend/lib/infrastructure/config/service-locator.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const constants = require('./constants');
+const environment = require('./environment');
+const JwtAccessTokenManager = require('../security/JwtAccessTokenManager');
+const UserSerializer = require('../../interfaces/serializers/UserSerializer');
+
+function buildBeans() {
+
+  const beans = {
+    accessTokenManager: new JwtAccessTokenManager(),
+    userSerializer: new UserSerializer(),
+  };
+
+  if (environment.database.dialect === constants.SUPPORTED_DATABASE.IN_MEMORY) {
+    const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
+    beans.userRepository = new UserRepositoryInMemory();
+  } else if (environment.database.dialect === constants.SUPPORTED_DATABASE.MONGO) {
+    const UserRepositoryMongo = require('../repositories/UserRepositoryMongo');
+    beans.userRepository = new UserRepositoryMongo();
+  } else if (environment.database.dialect === constants.SUPPORTED_DATABASE.POSTGRES) {
+    const UserRepositoryPostgres= require('../repositories/UserRepositoryPostgres');
+    beans.userRepository = new UserRepositoryPostgres();
+  } else {
+    const UserRepositorySQLite= require('../repositories/UserRepositorySQLite');
+    beans.userRepository = new UserRepositorySQLite();
+  }
+
+  return beans;
+}
+
+module.exports = buildBeans();
